72712e6315e64b877e586864cab1b2c204d2ad51
RaptorX72 <roka.tamas99@gmail.com>
2022-04-17 01:34:40 +0200

    Added score class changed unneeded static varaible

--- scorepager-desktop/Classes/FirebaseClient.cs
+++ scorepager-desktop/Classes/FirebaseClient.cs
@@ -3,13 +3,15 @@ using Google.Cloud.Firestore;
 using Firebase.Auth;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Collections.Generic;
 
 namespace scorepager_desktop.Classes {
 	class FirebaseClient {
-		private const string API_KEY = "AIzaSyAqCPbrsMc437mOHXRZF0T2hpcP9XMlQ0c";
+		private const string API_KEY = "AIzaSyDq0hFVdYy4r-whzaLX7gR92qTdMPyQZSM";
 		private static FirebaseClient client;
-		private static User user;
+		private User user;
 		private static bool loggedIn;
+		private FirestoreDb db;
 
 		public bool LoggedIn { get { return this.LoggedIn; } }
 
@@ -18,6 +20,7 @@ namespace scorepager_desktop.Classes {
 		public FirebaseClient() {
 			string path = AppDomain.CurrentDomain.BaseDirectory + @"..\..\Resources\JSON\firestoredb.json";
 			Environment.SetEnvironmentVariable("GOOGLE_APPLICATION_CREDENTIALS", path);
+			db = FirestoreDb.Create("scores");
 		}
 
 		public static FirebaseClient GetInstance() {
@@ -40,6 +43,21 @@ namespace scorepager_desktop.Classes {
 		public void Logout() {
 			user = null;
 			loggedIn = false;
+		}
+
+		public async Task<List<Score>> GetScores() {
+			QuerySnapshot scoresSnap = await db.Collection("scores").GetSnapshotAsync();
+			foreach (DocumentSnapshot docSnap in scoresSnap.Documents)
+			{
+				Score score = new Score(
+					docSnap.GetValue<string>("composer"),
+					docSnap.GetValue<string>("title"),
+					docSnap.GetValue<string>("uid"),
+					docSnap.GetValue<string>("url")
+				);
+				MessageBox.Show(score.ToString());
+			}
+			return null;
 		}
 	}
 }
---
+++ scorepager-desktop/Classes/Score.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace scorepager_desktop.Classes {
+    class Score {
+        private string composer;
+        private string title;
+        private string storageFolder;
+        private string url;
+
+        public string Composer { get => composer; }
+        public string Title { get => title; }
+        public string StorageFolder { get => storageFolder; }
+        public string Url { get => url; }
+
+        public Score(string composer, string title, string storageFolder, string url) {
+            this.composer = composer;
+            this.title = title;
+            this.storageFolder = storageFolder;
+            this.url = url;
+        }
+
+        public override string ToString()
+        {
+            return composer + " | " + title;
+        }
+    }
+}
